42463b3188cfb989a14fd4f6ac79b2dbfc5ae931
Brett Harvey <rbharvey@stanford.edu>
2019-02-21 16:23:47 -0800

    Without solution.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # testing-example
 Software testing example for Lecture 13 of CME 212 (2019)
+
+
+# googletest
+To install googletest on MacOS, run the following from this directory.
+* `cd googletest`
+* `mkdir build`
+* `cd build`
+* `cmake ..`
+* `make`
+* `make install`
--- src/periodicdomain.cpp
+++ src/periodicdomain.cpp
@@ -10,30 +10,6 @@ FnWithPeriodicDomain::FnWithPeriodicDomain(
 }
 
 bool FnWithPeriodicDomain::is_in_epigraph(double x, double y) {
-
-  double x_hi, y_hi, x_lo, y_lo;
-  auto sample_it = std::upper_bound(
-      samples_.begin(), samples_.end(), std::make_pair(x, 0),
-      [](const Sample &sample_one, const Sample &sample_two) {
-        return sample_one.first < sample_two.first;
-      });
-
-  if (sample_it == samples_.end()) {
-    sample_it = samples_.begin();
-  }
-  std::tie(x_hi, y_hi) = *sample_it;
-
-  if (sample_it == samples_.begin()) {
-    sample_it = samples_.end();
-  }
-  std::tie(x_lo, y_lo) = *(--sample_it);
-
-  // Remember, the domain of the function is periodic and is computed 
-  // as piecewise linear between sample points. Variable theta is
-  // fractionally how far x is along the interval [x_lo, x_hi).
-  double theta = (
-      std::fmod(x - x_lo + bound_, bound_)
-       / std::fmod(x_hi - x_lo + bound_, bound_));
-  double yhat = (1 - theta) * y_lo + theta * y_hi;
-  return y >= yhat;
+  // Your code here.
+  return true;
 }
